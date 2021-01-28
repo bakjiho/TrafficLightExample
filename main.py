823bd6625ab4003291a5a80a23da962a7807430a
bakjiho <wlgh2213@naver.com>
2021-01-29 04:55:50 +0900

    .

--- main.py
+++ main.py
@@ -61,56 +61,59 @@ def queue_debug():
 
 
 def findimg(imgpath, resultpath):
-    global net, classes, layer_names, output_layers
-    colors = np.random.uniform(0, 255, size=(len(classes), 3))
+    try:
+        global net, classes, layer_names, output_layers
+        colors = np.random.uniform(0, 255, size=(len(classes), 3))
 
-    # get image
-    img = cv2.imread(imgpath, cv2.IMREAD_COLOR)
+        # get image
+        img = cv2.imread(imgpath, cv2.IMREAD_COLOR)
 
-    height, width, channels = img.shape
+        height, width, channels = img.shape
 
-    # Detecting objects
-    blob = cv2.dnn.blobFromImage(img, 0.00392, (416, 416), (0, 0, 0), True, crop=False)
-    net.setPreferableBackend(cv2.dnn.DNN_BACKEND_OPENCV)
-    net.setPreferableTarget(cv2.dnn.DNN_TARGET_OPENCL)
+        # Detecting objects
+        blob = cv2.dnn.blobFromImage(img, 0.00392, (416, 416), (0, 0, 0), True, crop=False)
+        net.setPreferableBackend(cv2.dnn.DNN_BACKEND_OPENCV)
+        net.setPreferableTarget(cv2.dnn.DNN_TARGET_OPENCL)
 
-    net.setInput(blob)
+        net.setInput(blob)
 
-    outs = net.forward(output_layers)
+        outs = net.forward(output_layers)
 
-    # display info
-    class_ids = []
-    confidences = []
-    boxes = []
-    for out in outs:
-        for detection in out:
-            scores = detection[5:]
-            class_id = np.argmax(scores)
-            confidence = scores[class_id]
-            if confidence > 0.5:
-                # Object detected
-                center_x = int(detection[0] * width)
-                center_y = int(detection[1] * height)
-                w = int(detection[2] * width)
-                h = int(detection[3] * height)
-                # 좌표
-                x = int(center_x - w / 2)
-                y = int(center_y - h / 2)
-                boxes.append([x, y, w, h])
-                confidences.append(float(confidence))
-                class_ids.append(class_id)
-    indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
+        # display info
+        class_ids = []
+        confidences = []
+        boxes = []
+        for out in outs:
+            for detection in out:
+                scores = detection[5:]
+                class_id = np.argmax(scores)
+                confidence = scores[class_id]
+                if confidence > 0.5:
+                    # Object detected
+                    center_x = int(detection[0] * width)
+                    center_y = int(detection[1] * height)
+                    w = int(detection[2] * width)
+                    h = int(detection[3] * height)
+                    # 좌표
+                    x = int(center_x - w / 2)
+                    y = int(center_y - h / 2)
+                    boxes.append([x, y, w, h])
+                    confidences.append(float(confidence))
+                    class_ids.append(class_id)
+        indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
 
-    font = cv2.FONT_HERSHEY_PLAIN
-    for i in range(len(boxes)):
-        if i in indexes:
-            x, y, w, h = boxes[i]
-            label = str(classes[class_ids[i]])
-            color = colors[i]
-            cv2.rectangle(img, (x, y), (x + w, y + h), color, 2)
-            cv2.putText(img, label, (x, y + 30), font, 1.5, color, 2)
-    # outt.write(img)
-    cv2.imwrite(resultpath, img)
+        font = cv2.FONT_HERSHEY_PLAIN
+        for i in range(len(boxes)):
+            if i in indexes:
+                x, y, w, h = boxes[i]
+                label = str(classes[class_ids[i]])
+                color = colors[i]
+                cv2.rectangle(img, (x, y), (x + w, y + h), color, 2)
+                cv2.putText(img, label, (x, y + 30), font, 1.5, color, 2)
+        # outt.write(img)
+        cv2.imwrite(resultpath, img)
+    except Exception as e:
+        return
 
 @app.route('/')
 def home_page():
